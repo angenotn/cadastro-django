0de928934bedfaf3833ffa5f59b2943c60cebc8b
angenotn <angenot.n@gmail.com>
2022-10-14 23:43:16 -0300

    final do dia 13/10

---
+++ pessoa/models.py
@@ -0,0 +1,10 @@
+from asyncore import write
+from django.db import models
+
+class Pessoa(models.Model):
+    login = models.CharField(max_length = 256)
+    senha = models.CharField(max_length = 256, blank = True)
+    data_de_nascimento = models.DateField()
+
+def __str__(self) -> str:
+    return self().login
